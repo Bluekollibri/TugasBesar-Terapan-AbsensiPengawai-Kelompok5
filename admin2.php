3413e3386cd38e6a6f48d0f5b82caa157fdd9a91
itsbow <nurbagus1172@gmail.com>
2024-11-21 10:30:57 +0700

    Update admin2.php

--- admin2.php
+++ admin2.php
@@ -24,12 +24,7 @@
 
 <body class="animsition">
     <?php 
-    session_start();
-    if (!isset($_SESSION['username'])) {
-        header("location: index.php");
-    }else {
-        $username = $_SESSION['username'];  
-    }
+  
 
  ?>
     <div class="page-wrapper">
