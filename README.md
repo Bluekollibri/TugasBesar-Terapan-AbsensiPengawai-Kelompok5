039ca54769ce9887c6b67e301d5c82d534279c3b
itsbow <nurbagus1172@gmail.com>
2025-01-16 00:26:23 +0700

    bersih bersih

--- README.md
+++
@@ -1,9 +0,0 @@
-Kelompok 5 - Absensi Pengawai
-
-Back Log :
-Website : Nur,Raafi,Bryan
-Database : Morel & Bryan
-UI : Raafi & Morel
-
-Drive
-https://drive.google.com/drive/folders/1GXIlk6A6bEkrB3qliTHMxPSjdo-s50_2?usp=sharing
--- Udah bisa CRUD/resources/views/jabatans/create.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Tambah Jabatan</h1>
-<form action="{{ route('jabatans.store') }}" method="POST">
-    @csrf
-    <label for="nama">Nama Jabatan:</label>
-    <input type="text" name="nama" required>
-
-    <button type="submit">Simpan</button>
-</form>
