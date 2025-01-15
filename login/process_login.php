024b9f6ce2eb3ce336b34e6ae52e32b4c9bf7568
Morel_2172025 <2172025@maranatha.ac.id>
2025-01-16 00:36:36 +0700

    Update process_login.php

--- login/process_login.php
+++ login/process_login.php
@@ -1,22 +1,17 @@
 <?php
-$host = 'localhost'; // Ganti dengan host database Anda
-$dbname = 'nama_database'; // Ganti dengan nama database Anda
-$username = 'root'; // Ganti dengan username database Anda
-$password = ''; // Ganti dengan password database Anda
+$host = 'localhost'; 
+$dbname = 'nama_database'; 
+$username = 'root';
+$password = '';
 
-// Koneksi ke database
 $conn = new mysqli($host, $username, $password, $dbname);
-
-// Periksa koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
 
-// Ambil data dari form login
 $user = $_POST['email'];
 $pass = $_POST['password'];
 
-// Query untuk verifikasi login
 $sql = "SELECT * FROM karyawan WHERE nama = ? AND password = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $user, $pass);
@@ -25,11 +20,9 @@ $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     echo "Login berhasil! Selamat datang, $user.";
-    // Redirect ke halaman dashboard atau berikan respons sukses
     header("Location: Dashboard-admin.html");
 } else {
     echo "Login gagal! Username atau password salah.";
-    // Redirect kembali ke halaman login atau berikan pesan error
     header("Refresh: 2; URL=login_page.html");
 }
 
